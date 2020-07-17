14c92635446a71fab076a8923bbb447d11f5f84e
Mateus Ferreira <mateusanjos225@gmail.com>
2020-07-17 16:00:55 -0300

    Correção de bug na exclusão

---
+++ cobranca_spb/src/main/resources/static/js/cobranca.js
@@ -0,0 +1,17 @@
+$('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
+	
+	var button = $(event.relatedTarget);
+	var idTitulo = button.data('id'); 
+	var descricaoTitulo = button.data('descricao');
+	
+	var modal = $(this);
+	var	form = modal.find('form');
+	var action = form.data('url-base');
+	if(!action.endsWith('/')) {
+		action += '/';
+	}
+	
+	form.attr('action', action + idTitulo);
+	
+	modal.find('.modal-body').html('Tem certeza que deseja excluir o título <strong>'+ descricaoTitulo + '<strong>?');
+});
